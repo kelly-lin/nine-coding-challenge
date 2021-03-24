6153b765f3dbf4dbea0c924df06b0455e763903b
Kelly Lin <findkellylin@gmail.com>
2021-03-24 20:46:46 +1100

    refactor shows controller to remove nested if/else statements

--- controllers/shows.js
+++ controllers/shows.js
@@ -3,24 +3,26 @@ const { IsValidJsonString } = require('../utility/utility');
 const errors = require('../utility/errors');
 
 function getShows(req, res) {
-  if(IsValidJsonString(req.body)) {
-    // If the payload property does not exist then send back an error.
-    if(!req.body.hasOwnProperty('payload')) {
-      res.status(400).json(errors.noPayloadProperty);
-    } else {
-      const shows = req.body.payload;
-
-      if(!Array.isArray(shows)) {
-        res.status(400).json(errors.payloadNotArray);
-        return;
-      }
-
-      const filteredResponse = filterShows(shows);
-  
-      res.status(200).json(filteredResponse);
-    }
-  } else {
+  // If the body string is not a valid JSON string then respond with an error.
+  if(!IsValidJsonString(req.body)) {
     res.status(400).json(errors.badJsonRequest);
+    return;
   }
+
+  // If the payload property does not exist then send back an error.
+  if(!req.body.hasOwnProperty('payload')) {
+    res.status(400).json(errors.noPayloadProperty);
+    return;
+  }
+
+  const shows = req.body.payload;
+
+  if(!Array.isArray(shows)) {
+    res.status(400).json(errors.payloadNotArray);
+    return;
+  }
+
+  const filteredResponse = filterShows(shows);
+  res.status(200).json(filteredResponse);
 }
 module.exports.getShows = getShows;
