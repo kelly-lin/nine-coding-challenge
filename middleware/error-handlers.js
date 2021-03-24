444cc5d3032840abc4003a2dfb2acd93fb2fd947
Kelly Lin <findkellylin@gmail.com>
2021-03-24 21:55:35 +1100

    deleted middleware

--- middleware/error-handlers.js
+++
@@ -1,8 +0,0 @@
-function handleJsonError(err, req, res, next) {
-  if(err) {
-    res.status(400).send(errors.badJsonRequest);
-  } else {
-    next();
-  }
-}
-module.exports.handleJsonError = handleJsonError;
